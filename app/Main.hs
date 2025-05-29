5cd7152e7a08965a9fe061094328971237c92e3e
Victor <slavco2.p@gmail.com>
2025-05-29 17:58:36 +0300

    Move separator into function

--- app/Main.hs
+++ app/Main.hs
@@ -39,22 +39,24 @@ runInterpreter filePath isDebug = do
     Left er -> putStrLn (show er)
     Right pr -> do
       ifDebug (putStrLn (show pr))
-      ifDebug (putStrLn (take 40 $ repeat '-'))
+      ifDebug printSeparator
       ifDebug (putStrLn "Analyzing parsed program...")
       case applyAnalyzer pr of
         Left er -> putStrLn (show er)
         Right a -> do
           ifDebug (putStrLn (show a))
-          ifDebug (putStrLn (take 40 $ repeat '-'))
+          ifDebug printSeparator
           ifDebug (putStrLn "Interpreting program...")
           ifDebug (putStrLn "Program output:")
           intrRes <- applyInterpreter pr
           case intrRes of
             Left er -> putStrLn (show er)
             Right i -> do
-              ifDebug (putStrLn ("\n" ++ (take 40 $ repeat '-')))
+              ifDebug (putStrLn "")
+              ifDebug printSeparator
               ifDebug (putStrLn "Final interpreter state:")
               ifDebug (putStrLn (show i))
   hClose handle
  where
   ifDebug fn = if isDebug then fn else pure ()
+  printSeparator = putStrLn (take 40 $ repeat '-')
